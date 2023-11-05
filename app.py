f1919e716f70d8b5fb53e0e65c606a4f22d5b789
hojin7958 <imkoreanguy@gmail.com>
2023-11-06 08:55:57 +0900

    new file:   "[\353\252\250\354\225\204\353\263\264\352\270\260] \354\235\264\354\240\234\353\212\224 \354\234\240\353\252\205 \352\260\200\354\210\230, \353\262\204\354\212\244\355\202\271\352\271\214\354\247\200 \354\231\204\353\262\275\355\225\234 \354\235\264\353\254\264\354\247\204(Lee Mujin) \353\205\270\353\236\230\353\252\250\354\235\214 #\354\230\244\355\224\210\353\247\210\354\235\264\355\201\254.mp3" 	new file:   app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import yt_dlp
+import streamlit_ext as ste
+
+
+st.header("유튜브 다운로드")
+
+if 'video_title' not in st.session_state:
+    st.session_state['video_title'] = ''
+
+
+url = st.text_input("주소를 입력해주세요")
+
+print(url)
+
+def download_audio(link):
+  global video_title
+  with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '%(title)s.mp3'}) as video:
+    info_dict = video.extract_info(link, download = True)
+    video_title = info_dict['title']
+    print(video_title)
+    video.download(link)   
+    print("Successfully Downloaded - see local folder on Google Colab")
+    st.session_state['video_title'] = video_title
+
+
+
+downloaded = st.button('다운로드받기', on_click=download_audio, args=[url])
+
+if downloaded:
+    st.write(st.session_state['video_title'])
+    file_name = st.session_state['video_title'] + ".mp3"
+
+    with open(file_name, "rb") as file:
+        btn = ste.download_button(
+                label='다운로드',
+                data=file,
+                file_name=file_name,
+                mime="audio/mp3",
+            )
+
