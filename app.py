7c0b8eb42e81286040412bb8e6990e66fb4523fd
hojin7958 <imkoreanguy@gmail.com>
2023-11-07 18:09:06 +0900

    deleted:    "[\353\252\250\354\225\204\353\263\264\352\270\260] \354\235\264\354\240\234\353\212\224 \354\234\240\353\252\205 \352\260\200\354\210\230, \353\262\204\354\212\244\355\202\271\352\271\214\354\247\200 \354\231\204\353\262\275\355\225\234 \354\235\264\353\254\264\354\247\204(Lee Mujin) \353\205\270\353\236\230\353\252\250\354\235\214 #\354\230\244\355\224\210\353\247\210\354\235\264\355\201\254.mp3" 	modified:   app.py 	deleted:    temp.mp3

--- app.py
+++ app.py
@@ -1,46 +1,88 @@
 import streamlit as st
 import yt_dlp
 import streamlit_ext as ste
+import os
+import glob
+import re
+import emoji
+
 
 
 st.header("유튜브 다운로드")
 
+
+pattern = re.compile(f'[^ .,?!/@$%~％·∼()\x00-\x7Fㄱ-ㅣ가-힣]+')
+url_pattern = re.compile(
+    r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)')
+
+def clean(x): 
+    x = pattern.sub(' ', x)
+    x = emoji.replace_emoji(x, replace='') #emoji 삭제
+    x = url_pattern.sub('', x)
+    x = x.strip()
+    return x
+
+
+def delete_files():
+    for(path,dir,files)in os.walk(os.getcwd()) :#경로
+        for filename in files :
+            ext=os.path.splitext(filename)[-1]
+            if ext=='.mp3':#확장자명
+                print("%s\\%s"%(path,filename))
+                os.remove("%s\\%s"%(path,filename))
+
+
+
 if 'video_title' not in st.session_state:
     st.session_state['video_title'] = ''
+    delete_files()
 
-password = '7958'
-
-check_password = st.text_input("비밀번호를 입력해주세요")
-
-if check_password == '7958':
-
-    url = st.text_input("동영상 주소를 입력해주세요")
-
-    print(url)
-
-    def download_audio(link):
-        global video_title
-        with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': 'temp.mp3'}) as video:
-            info_dict = video.extract_info(link, download = True)
-            video_title = info_dict['title']
-            print(video_title)
-            video.download(link)   
-            print("Successfully Downloaded - see local folder on Google Colab")
-            st.session_state['video_title'] = video_title
+if 'file_name' not in st.session_state:
+    st.session_state['file_name'] = ''
 
 
+url = st.text_input("동영상 주소를 입력해주세요")
 
-    downloaded = st.button('주소점검하기(1단계)', on_click=download_audio, args=[url])
+print(url)
 
-    if downloaded:
-        st.write(st.session_state['video_title'])
-        file_name = "temp.mp3"
+# 테스트
+# https://youtu.be/ToASX6axGuw?si=kpicX8QyYVExBjQF
 
-        with open(file_name, "rb") as file:
-            btn = ste.download_button(
-                    label='다운로드',
-                    data=file,
-                    file_name=file_name,
-                    mime="audio/mp3",
-                )
 
+def download_audio(link):
+    global video_title
+
+    with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio'}) as video1:
+        info_dict = video1.extract_info(link, download = False)
+        video_title = info_dict['title']
+        st.session_state['video_title'] = video_title
+        file_name = clean(video_title)
+        st.session_state['file_name'] = file_name
+
+
+    with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': f'{file_name}.mp3'}) as video:
+        info_dict = video.extract_info(link, download = True)
+        video_title = info_dict['title']
+        st.session_state['video_title'] = video_title
+        file_name = clean(video_title)
+        st.session_state['file_name'] = file_name
+        print(file_name)
+        video.download(link)   
+        print("Successfully Downloaded - see local folder on Google Colab")
+
+
+downloaded = st.button('주소점검하기(1단계)', on_click=download_audio, args=[url])
+
+if downloaded:
+    st.write(st.session_state['video_title'])
+    file_name = st.session_state['file_name']+'.mp3'
+
+    with open(file_name, "rb") as file:
+        btn = ste.download_button(
+                label='다운로드',
+                data=file,
+                file_name=file_name,
+                mime="audio/mp3"
+
+            )
+        
